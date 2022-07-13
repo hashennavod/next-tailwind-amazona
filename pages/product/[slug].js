5836c58b81ef471463200192dd239d3a27695899
Hashen <hashengunawardana@gmail.com>
2022-07-13 23:06:09 +0530

    finish cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import {useRouter} from 'next/router'
+import  {useRouter} from 'next/router'
 import React, { useContext } from 'react'
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
@@ -8,7 +8,7 @@ import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
     const{state,dispatch}=useContext(Store);
-
+    const router=useRouter();
     const{ query } = useRouter();
     const{slug}=query;
     const product=data.products.find((x)=>x.slug==slug);
@@ -25,6 +25,7 @@ export default function ProductScreen() {
         return;
       }
       dispatch({type:'CART_ADD_ITEM',payload:{...product,quantity}});
+      router.push('/cart');
     }
 
   return (
